67afed2ede3e17c7cab8270a87d9fdbdc29a4ead
wangbin <three6one@163.com>
2017-12-14 16:06:44 +0800

    1214

--- app/src/main/java/com/vegetablestrading/activity/MainActivity.java
+++ app/src/main/java/com/vegetablestrading/activity/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         PublicUtils.app_height = wm.getDefaultDisplay().getHeight();
 //        startActivity(new Intent(this, ActivatedActivity.class));
         checkForUpdate(this);
-
+        Toast.makeText(getApplicationContext(), "这是分支", Toast.LENGTH_LONG).show();
     }
 
     /**
